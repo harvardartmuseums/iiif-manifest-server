3dbdf8e9ca6798ae439ef5d0177c767628ff41b4
Jeff Steward <steward.jeff@gmail.com>
2015-07-02 17:45:54 -0400

    forgot to import settings

--- manifests/views.py
+++ manifests/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.conf import settings
 from django.http import HttpResponse, HttpResponseRedirect
 from manifests import huam
 from manifests import models
