1b9bf8b80258b3c5d9b436b419af7233a0f0ee6a
Jeff Steward <steward.jeff@gmail.com>
2017-07-14 16:22:53 -0400

    move secret key to env

--- metadata_server/settings.py
+++ metadata_server/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kctxlari&k!f(#eu(q*d#*c(sg2&3f)sloy9jfp-!xmxtbu(d@'
+SECRET_KEY = os.environ.get('PYTHON_DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
