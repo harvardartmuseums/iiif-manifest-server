503b49c2f0d123b719fce62f2c5ce4f45c70ae81
Jeff Steward <steward.jeff@gmail.com>
2017-08-04 12:27:24 -0400

    remove move some stray comments

--- metadata_server/urls.py
+++ metadata_server/urls.py
@@ -4,9 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'metadata_server.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
     url(r'^manifests/', include('manifests.urls')),
     url(r'^viewers/', include('viewers.urls')),
     url(r'^admin/', include(admin.site.urls)),
