713e261b8c720a43552dfbafadbafccecb0cfc0b
Jeff Steward <steward.jeff@gmail.com>
2020-03-23 17:42:46 -0400

    initial implementation of mirador 3

--- viewers/urls.py
+++ viewers/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from viewers import views
 
 urlpatterns = patterns('',
+    url(r'^(?P<viewer_type>(mirador))/(?P<viewer_version>(v1|v2|v3))$', views.view, name='view'),
     url(r'^(?P<viewer_type>(mirador))$', views.view, name='view'),
 )
--- viewers/views.py
+++ viewers/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 
-def view(request, viewer_type):
-	if (viewer_type == "mirador"):
-		return render(request, 'viewers/mirador.html')
+def view(request, viewer_type, viewer_version='v2'):
+	if (viewer_type == 'mirador'):
+		if (viewer_version == 'v2'):
+			return render(request, 'viewers/mirador-v2.html')
+		elif (viewer_version == 'v3'):
+			return render(request, 'viewers/mirador-v3.html')
+		else:
+			return render(request, 'viewers/mirador-v2.html')
+
+	else:
+		return render(request, 'viewers/mirador-v2.html')
